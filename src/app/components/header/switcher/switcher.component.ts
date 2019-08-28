9abd29b7f3acda68d7ca238c1c7df56d2fd1c9a0
Moe Green <gearmobile@gmail.com>
2019-08-28 16:12:45 +0300

    feat: fill header component

--- src/app/components/header/switcher/switcher.component.ts
+++ src/app/components/header/switcher/switcher.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'fw-switcher',
   templateUrl: './switcher.component.html',
-  styleUrls: ['./switcher.component.scss']
+  styleUrls: ['./switcher.component.scss'],
 })
 export class SwitcherComponent implements OnInit {
+  constructor(private translate: TranslateService, private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
   }
 
+  switchLang(lang: string): void {
+    this.router.navigateByUrl(`${lang}`);
+  }
 }
